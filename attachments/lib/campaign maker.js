1c6954d7e9ef9bbd01fc31f72d0008e6def65668
Paul Iannazzo <somethingitalian@gmail.com>
2011-10-17 17:54:58 -0400

    i don't know what i changed...

--- attachments/lib/campaign maker.js
+++ attachments/lib/campaign maker.js
@@ -1,16 +1,17 @@
 function doc_setup(){
-	var dates = $( "#startDate, #endDate" ).datepicker({
-	    defaultDate: "+1w",
-	    changeMonth: true,
-	    numberOfMonths: 3,
-	    onSelect: function( selectedDate ) {
-		var option = this.id == "from" ? "minDate" : "maxDate",
-		instance = $( this ).data( "datepicker" ),
-		date = $.datepicker.parseDate(
-		    instance.settings.dateFormat ||
-			$.datepicker._defaults.dateFormat,
-		    selectedDate, instance.settings );
-		dates.not( this ).datepicker( "option", option, date );
-	    }
-	});    
+    var dates = $( "#startDate, #endDate" )
+	.datepicker({
+			defaultDate: "+1w",
+			changeMonth: true,
+			numberOfMonths: 3,
+			onSelect: function( selectedDate ) {
+			    var option = this.id == "from" ? "minDate" : "maxDate",
+			    instance = $( this ).data( "datepicker" ),
+			    date = $.datepicker.parseDate(
+				instance.settings.dateFormat ||
+				    $.datepicker._defaults.dateFormat,
+				selectedDate, instance.settings );
+			    dates.not( this ).datepicker( "option", option, date );
+			}
+		    });    
 };
